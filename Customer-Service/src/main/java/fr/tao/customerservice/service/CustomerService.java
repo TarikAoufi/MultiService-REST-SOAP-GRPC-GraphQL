0f89a5bbbb3b1b983f068d4148aa5c698f6fba6a
Tarik AOUFI <t_aoufi@hotmail.com>
2024-05-05 22:59:07 +0200

    Update CustomerService.java

--- Customer-Service/src/main/java/fr/tao/customerservice/service/CustomerService.java
+++ Customer-Service/src/main/java/fr/tao/customerservice/service/CustomerService.java
@@ -15,42 +15,42 @@ import jakarta.persistence.NoResultException;
  */
 public interface CustomerService {
 	
-	/**
+    /**
      * Retrieves a list of all customers.
      *
      * @return A list of CustomerDto objects representing customers.
      * @throws Exception if there is an error during the operation.
      */
-	public List<CustomerDto> getAllCustomer() throws Exception;
+    public List<CustomerDto> getAllCustomer() throws Exception;
 	
-	/**
+    /**
      * Retrieves a customer by their unique identifier.
      *
      * @param customerId The unique identifier of the customer.
      * @return A CustomerDto object representing the customer.
      * @throws CustomerNotFoundException if the customer with the specified ID is not found.
      */
-	public CustomerDto getCustomerById(Long customerId) throws CustomerNotFoundException;
+    public CustomerDto getCustomerById(Long customerId) throws CustomerNotFoundException;
 	
-	/**
+    /**
      * Saves customer information.
      *
      * @param customerDto The CustomerDto object containing customer information to be saved.
      * @return The saved CustomerDto object.
      * @throws Exception if there is an error during the operation.
      */
-	public CustomerDto saveCustomer(CustomerDto customerDto) throws Exception;	
+    public CustomerDto saveCustomer(CustomerDto customerDto) throws Exception;	
 	
-	/**
+    /**
      * Retrieves a list of customers whose names contain the specified keyword.
      *
      * @param name The keyword to search for in customer names.
      * @return A list of CustomerDto objects representing matching customers.
      * @throws NoResultException if no matching customers are found.
      */
-	public List<CustomerDto> getCustomersByNameContains(String name) throws NoResultException;
+    public List<CustomerDto> getCustomersByNameContains(String name) throws NoResultException;
 	
-	/**
+    /**
      * Updates customer information by their unique identifier.
      *
      * @param customerId The unique identifier of the customer to be updated.
@@ -58,14 +58,14 @@ public interface CustomerService {
      * @return The updated CustomerDto object.
      * @throws Exception if there is an error during the operation.
      */
-	public CustomerDto updateCustomer(Long customerId, CustomerDto customerDto) throws Exception;
+    public CustomerDto updateCustomer(Long customerId, CustomerDto customerDto) throws Exception;
 	
-	/**
+    /**
      * Deletes a customer by their unique identifier.
      *
      * @param customerId The unique identifier of the customer to be deleted.
      * @throws Exception if there is an error during the operation.
      */
-	public void deleteCustomer(Long customerId) throws Exception;
+    public void deleteCustomer(Long customerId) throws Exception;
 	
 }
