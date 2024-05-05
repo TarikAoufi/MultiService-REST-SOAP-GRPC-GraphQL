d89306de2107dc38a8c07a232c3ed4f1a2e79770
Tarik AOUFI <t_aoufi@hotmail.com>
2024-05-06 01:28:49 +0200

    Update RemoteApiProperties.java

--- Customer-Service-Client/src/main/java/fr/tao/customerserviceclient/common/RemoteApiProperties.java
+++ Customer-Service-Client/src/main/java/fr/tao/customerserviceclient/common/RemoteApiProperties.java
@@ -19,29 +19,29 @@ import lombok.Getter;
 @PropertySource("classpath:remote_api.properties")
 public class RemoteApiProperties {
 	
-	@Value("${api.base_url}")
-	private String baseUrl; // Base URL for the remote API.
+    @Value("${api.base_url}")
+    private String baseUrl; // Base URL for the remote API.
 	
-	@Value("${api.uri_customers}")
-	private String uriCustomers; // URI for accessing customers.
+    @Value("${api.uri_customers}")
+    private String uriCustomers; // URI for accessing customers.
 	
-	@Value("${api.uri_customerById}")
-	private String uriCustomerById; // URI for accessing a specific customer.
+    @Value("${api.uri_customerById}")
+    private String uriCustomerById; // URI for accessing a specific customer.
 	
-	@Value("${graphql.uri}")
-	private String uriGraphql; // URI for GraphQL endpoint.
+    @Value("${graphql.uri}")
+    private String uriGraphql; // URI for GraphQL endpoint.
 	
-	@Value("${soap.url}")
-	private String soapUrl; // URL for SOAP service.
+    @Value("${soap.url}")
+    private String soapUrl; // URL for SOAP service.
 
-	/**
+    /**
      * Constructs and returns the full URL for the remote API by appending the provided URI to the base URL.
      *
      * @param uri The URI to be appended to the base URL.
      * @return The full remote API URL.
      */
-	public String getRemoteApiUrl(String uri) {
-		return this.getBaseUrl() + uri;
-	}
+    public String getRemoteApiUrl(String uri) {
+	return this.getBaseUrl() + uri;
+    }
 
 }
