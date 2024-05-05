0deeede2ec40d30920a5a6509818cef24952c6b2
Tarik AOUFI <t_aoufi@hotmail.com>
2024-05-05 22:30:48 +0200

    Update CustomerGrpcService.java

--- Customer-Service/src/main/java/fr/tao/customerservice/api/grpc/CustomerGrpcService.java
+++ Customer-Service/src/main/java/fr/tao/customerservice/api/grpc/CustomerGrpcService.java
@@ -31,56 +31,56 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @Slf4j
 public class CustomerGrpcService extends CustomerServiceGrpc.CustomerServiceImplBase {
 	
-	/**
+    /**
      * The service responsible for managing customer data.
      */
-	@NonNull private final CustomerService customerService;
-	/**
+    @NonNull private final CustomerService customerService;
+    /**
      * A mapper for converting between customer DTOs and Protos.
      */
-	@NonNull private final CustomerMapper customerMapper;
+    @NonNull private final CustomerMapper customerMapper;
 	
-	/**
+    /**
      * Retrieves all customers and sends the response to the client.
      *
      * @param request          Empty request from the client.
      * @param responseObserver StreamObserver for sending the response.
      */
-	@Override
-	public void getAllCustomers(Empty request, StreamObserver<CustomerList> responseObserver) {
-		try {
-			log.info("Handling getAllCustomers via gRPC request.");
-			var customers = customerService.getAllCustomer();
+    @Override
+    public void getAllCustomers(Empty request, StreamObserver<CustomerList> responseObserver) {
+	try {
+	    log.info("Handling getAllCustomers via gRPC request.");
+	    var customers = customerService.getAllCustomer();
 			
-			// Converting customers to Protos
-			var customerProtos = customers.stream()
-	                .map(customerMapper::toCustomerProto)
-	                .toList();
+	    // Converting customers to Protos
+	    var customerProtos = customers.stream()
+	            .map(customerMapper::toCustomerProto)
+	            .toList();
 			
-			var response = CustomerList.newBuilder()
-	                .addAllCustomers(customerProtos)
-	                .build();
+	    var response = CustomerList.newBuilder()
+	            .addAllCustomers(customerProtos)
+	            .build();
 			
-			// Sending response to client
-			responseObserver.onNext(response);
-	        responseObserver.onCompleted();
+	    // Sending response to client
+	    responseObserver.onNext(response);
+            responseObserver.onCompleted();
 	        
-	        log.info("getAllCustomers - gRPC request: Successfully retrieved all customers.");
-		} catch (Exception e) {
+	    log.info("getAllCustomers - gRPC request: Successfully retrieved all customers.");
+	} catch (Exception e) {
             log.error("getAllCustomers - gRPC request: Error while retrieving all customers.", e);
             responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());      
-		}         
-	}
+	}         
+    }
 	
-	/**
+    /**
      * Retrieves a customer by ID and sends the response to the client.
      *
      * @param request          CustomerId request specifying the customer ID.
      * @param responseObserver StreamObserver for sending the response.
      */
-	@Override
-	public void getCustomerById(CustomerId request, StreamObserver<CustomerResponse> responseObserver) {
-		try {
+    @Override
+    public void getCustomerById(CustomerId request, StreamObserver<CustomerResponse> responseObserver) {
+	try {
             log.info("Handling getCustomerById, via gRPC request, for ID: {}", request.getId());
             var customerDto = customerService.getCustomerById(Long.valueOf(request.getId()));
             responseObserver.onNext(customerMapper.toCustomerProto(customerDto));
@@ -90,18 +90,18 @@ public class CustomerGrpcService extends CustomerServiceGrpc.CustomerServiceImpl
             log.error("getCustomerById - gRPC request: Customer with ID {} not found.", request.getId());
             responseObserver.onError(Status.NOT_FOUND.withDescription(e.getMessage()).asRuntimeException());
         }
-	}
+    }
 	
-	/**
+    /**
      * Creates a new customer and sends the response to the client.
      *
      * @param request          CustomerRequest with customer information.
      * @param responseObserver StreamObserver for sending the response.
      */
-	@Override
-	public void createCustomer(CustomerRequest request, StreamObserver<CustomerResponse> responseObserver) {
-		try {
-			log.info("Handling createCustomer via gRPC request.");
+    @Override
+    public void createCustomer(CustomerRequest request, StreamObserver<CustomerResponse> responseObserver) {
+	try {
+	    log.info("Handling createCustomer via gRPC request.");
             CustomerDto customerDto = customerService.saveCustomer(customerMapper.fromCustomerRequest(request));
             responseObserver.onNext(customerMapper.toCustomerProto(customerDto));
             responseObserver.onCompleted();
@@ -110,6 +110,6 @@ public class CustomerGrpcService extends CustomerServiceGrpc.CustomerServiceImpl
             log.error("createCustomer - gRPC request: Error while creating a new customer.", e);
             responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
         }
-	}
+    }
 	
 }
