63578f5f78bf9e453dc1c40206db24093211b3e0
Tarik AOUFI <t_aoufi@hotmail.com>
2024-05-06 01:44:02 +0200

    Update CustomerGraphQLClient.java

--- Customer-Service-Client/src/main/java/fr/tao/customerserviceclient/service/graphql/CustomerGraphQLClient.java
+++ Customer-Service-Client/src/main/java/fr/tao/customerserviceclient/service/graphql/CustomerGraphQLClient.java
@@ -36,9 +36,9 @@ import reactor.core.publisher.Mono;
 public class CustomerGraphQLClient {
 	
 	
-	/**
-	 * Configuration properties for the remote API.	
-	 */
+    /**
+     * Configuration properties for the remote API.	
+     */
     @NonNull 
     private final RemoteApiProperties remoteApiProperties;	
     
@@ -62,11 +62,11 @@ public class CustomerGraphQLClient {
      */
     public Mono<List<Customer>> getAllCustomersGQLHttpGQl() {
     	log.info("Fetching all customers using HTTP GraphQL client.");
-		return httpGraphQlClient
-				.document(MessageUtil.GET_ALL_CUSTOMER_QUERY)
-				.retrieve(MessageUtil.ALL_CUSTOMER_SERVICE_NAME)
-				.toEntityList(Customer.class);
-	}
+	return httpGraphQlClient
+		.document(MessageUtil.GET_ALL_CUSTOMER_QUERY)
+		.retrieve(MessageUtil.ALL_CUSTOMER_SERVICE_NAME)
+		.toEntityList(Customer.class);
+    }
     
     /**
      * Retrieves a customer by their ID using HTTP-based GraphQL.
@@ -76,12 +76,12 @@ public class CustomerGraphQLClient {
      */
     public Mono<Customer> getCustomerByIdGQLHttpGQl(Long id) {
     	log.info("Fetching customer by ID {} using HTTP GraphQL client.", id);
-		return httpGraphQlClient
-				.document(MessageUtil.GET_CUSTOMER_BY_ID_QUERY.formatted(id))
-				.variable("id", id)
-				.retrieve(MessageUtil.CUSTOMER_BY_ID_SERVICE_NAME)
-				.toEntity(Customer.class);
-	}
+	return httpGraphQlClient
+		.document(MessageUtil.GET_CUSTOMER_BY_ID_QUERY.formatted(id))
+		.variable("id", id)
+		.retrieve(MessageUtil.CUSTOMER_BY_ID_SERVICE_NAME)
+		.toEntity(Customer.class);
+    }
     
     /**
      * Retrieves a list of all customers using WebClient-based GraphQL.
@@ -89,35 +89,35 @@ public class CustomerGraphQLClient {
      * @return A list of Customer objects.
      * @throws Exception if an error occurs during the GraphQL query execution.
      */
-	public List<Customer> getAllCustomersGQLWebClient() throws Exception {
-		log.info("Fetching all customers using WebClient.");
-		String query = MessageUtil.GET_ALL_CUSTOMER_QUERY;
-		var result = executeGraphQLQuery(query, MessageUtil.ALL_CUSTOMER_SERVICE_NAME);		
-		var typeToken = new TypeToken<List<Customer>>() {};
+    public List<Customer> getAllCustomersGQLWebClient() throws Exception {
+	log.info("Fetching all customers using WebClient.");
+	String query = MessageUtil.GET_ALL_CUSTOMER_QUERY;
+	var result = executeGraphQLQuery(query, MessageUtil.ALL_CUSTOMER_SERVICE_NAME);		
+	var typeToken = new TypeToken<List<Customer>>() {};
 		
-		return result != null
-		        ? new Gson().fromJson(result.toString(), typeToken.getType())
-		        : Collections.emptyList();
-	}
+	return result != null
+	        ? new Gson().fromJson(result.toString(), typeToken.getType())
+	        : Collections.emptyList();
+    }
 	
-	/**
+    /**
      * Retrieves a customer by their ID using WebClient-based GraphQL.
      *
      * @param id The ID of the customer to retrieve.
      * @return A Customer object.
      * @throws Exception if an error occurs during the GraphQL query execution.
      */
-	public Customer getCustomerByIdGQLWebClient(Long id) throws Exception {
-		log.info("Fetching customer by ID {} using WebClient.", id);
-		String query = MessageUtil.GET_CUSTOMER_BY_ID_QUERY.formatted(id);
-		var result = executeGraphQLQuery(query, MessageUtil.CUSTOMER_BY_ID_SERVICE_NAME);
+    public Customer getCustomerByIdGQLWebClient(Long id) throws Exception {
+	log.info("Fetching customer by ID {} using WebClient.", id);
+	String query = MessageUtil.GET_CUSTOMER_BY_ID_QUERY.formatted(id);
+	var result = executeGraphQLQuery(query, MessageUtil.CUSTOMER_BY_ID_SERVICE_NAME);
 		
-		return Optional.ofNullable(result)
-		        .map(json -> new Gson().fromJson(json.toString(), Customer.class))
-		        .orElse(null);
-	}
+	return Optional.ofNullable(result)
+	        .map(json -> new Gson().fromJson(json.toString(), Customer.class))
+	        .orElse(null);
+    }
     
-	/**
+    /**
      * Executes a GraphQL query using the WebClient.
      *
      * @param query The GraphQL query to execute.
